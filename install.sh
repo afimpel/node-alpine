4b818cf748520f8b48f70500b5a01be0f5dd4cb0
Alvaro Fimpel <afimpel@afimpel.com>
2023-03-01 01:11:32 -0300

    yarn 2

--- install.sh
+++ install.sh
@@ -5,8 +5,9 @@ NODE_VERSION=$(jq -r '.[0].version' /tmp/release.json);
 wget --no-cache "https://unofficial-builds.nodejs.org/download/release/$NODE_VERSION/node-$NODE_VERSION-linux-x64-musl.tar.xz" -O /tmp/node-current.tar.xz
 tar -xJf "/tmp/node-current.tar.xz" -C /usr/local --strip-components=1 --no-same-owner
 ln -s /usr/local/bin/node /usr/local/bin/nodejs
+corepack enable
 npm install --global yarn nodemon pm2 npm
-
+yarn set version stable
 echo "nodeJS: $(node --version)"
 echo "NPM: $(npm --version)"
 echo "yarn: $(yarn --version)"
