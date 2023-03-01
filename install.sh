636f113a6bc345d5278623082f93bdce6a3b8f1a
Alvaro Fimpel <afimpel@afimpel.com>
2023-03-01 01:05:31 -0300

    yarn nodemon pm2 npm

--- install.sh
+++ install.sh
@@ -5,10 +5,9 @@ NODE_VERSION=$(jq -r '.[0].version' /tmp/release.json);
 wget --no-cache "https://unofficial-builds.nodejs.org/download/release/$NODE_VERSION/node-$NODE_VERSION-linux-x64-musl.tar.xz" -O /tmp/node-current.tar.xz
 tar -xJf "/tmp/node-current.tar.xz" -C /usr/local --strip-components=1 --no-same-owner
 ln -s /usr/local/bin/node /usr/local/bin/nodejs
-npm install --global yarn nodemon pm2
+npm install --global yarn nodemon pm2 npm
 
-node --version
-npm --version
-yarn --version
-nodemon --version
-pm2 --version
+echo "nodeJS: $(node --version)"
+echo "NPM: $(npm --version)"
+echo "yarn: $(yarn --version)"
+echo "nodemon: $(nodemon --version)"
