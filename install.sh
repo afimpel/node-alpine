7b87fffb162eadd49e56bcf1801492609e1b083f
Alvaro Fimpel <afimpel@afimpel.com>
2023-03-02 00:50:20 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 apk add jq bash curl
-rm -vf /usr/local/bin/nod* /usr/local/bin/yar*
+rm -f /usr/local/bin/nod* /usr/local/bin/yar*
 wget --no-cache https://unofficial-builds.nodejs.org/download/release/index.json -O /tmp/release.json 
 NODE_VERSION=$(jq -r '.[0].version' /tmp/release.json);
+
+echo "Install Current Version ..."
 wget --no-cache "https://unofficial-builds.nodejs.org/download/release/$NODE_VERSION/node-$NODE_VERSION-linux-x64-musl.tar.xz" -O /tmp/node-current.tar.xz
 tar -xJf "/tmp/node-current.tar.xz" -C /usr/local --strip-components=1 --no-same-owner
 ln -s /usr/local/bin/node /usr/local/bin/nodejs
+echo "nodeJS: $(node --version)"
+echo "NPM: $(npm --version)"
 corepack enable
-npm install --global nodemon pm2 npm yarn
+
+echo "Install nodemon and update npm ..."
+npm install --global nodemon npm
 yarn set version stable
+
+echo "versions ..."
 echo "nodeJS: $(node --version)"
 echo "NPM: $(npm --version)"
 echo "yarn: $(yarn --version)"
