cd63b85f74929de743a66022b7861183c8e9bf60
Alvaro Fimpel <afimpel@afimpel.com>
2023-03-02 01:09:46 -0300

    Current NodeJS

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # nodejs-alpine
 install node to alpine linux,
 
-run as ROOT
-``` wget --no-cache -O - https://raw.githubusercontent.com/afimpel/nodejs-alpine/main/install.sh | sh ``` 
+!!! run as ROOT !!! 
+## Current Version ...
+
+``` wget --no-cache -O - https://raw.githubusercontent.com/afimpel/nodejs-alpine/main/install-current.sh | sh ``` 
--- install-current.sh
+++ install-current.sh
@@ -3,20 +3,23 @@ apk add jq bash curl
 rm -f /usr/local/bin/nod* /usr/local/bin/yar*
 wget --no-cache https://unofficial-builds.nodejs.org/download/release/index.json -O /tmp/release.json 
 NODE_VERSION=$(jq -r '.[0].version' /tmp/release.json);
-
-echo "Install Current Version ..."
+NODE_DATE=$(jq -r '.[0].date' /tmp/release.json);
+clear
+echo -e "Current NodeJS: ${NODE_VERSION} | ${NODE_DATE} \n"
+echo -e "Install Current Version ... \n"
 wget --no-cache "https://unofficial-builds.nodejs.org/download/release/$NODE_VERSION/node-$NODE_VERSION-linux-x64-musl.tar.xz" -O /tmp/node-current.tar.xz
 tar -xJf "/tmp/node-current.tar.xz" -C /usr/local --strip-components=1 --no-same-owner
 ln -s /usr/local/bin/node /usr/local/bin/nodejs
+clear
 echo "nodeJS: $(node --version)"
 echo "NPM: $(npm --version)"
 corepack enable
 
-echo "Install nodemon and update npm ..."
+echo -e  "\nInstall nodemon and update npm ... \n"
 npm install --global nodemon npm
 yarn set version stable
 
-echo "versions ..."
+echo -e "\n versions ..."
 echo "nodeJS: $(node --version)"
 echo "NPM: $(npm --version)"
 echo "yarn: $(yarn --version)"
